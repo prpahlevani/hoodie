ae6cc20aaec831193ef86112e5668b941b21771f
Pooria Pahlevani <pooria.cpp@gmail.com>
2023-04-04 16:32:21 +0330

    add facade class

---
+++ src/Facades/ApiResponderFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Motrack\Hoodie\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Motrack\Hoodie\Interfaces\ApiResponderInterface;
+
+class ApiResponderFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return ApiResponderInterface::class;
+    }
+}
